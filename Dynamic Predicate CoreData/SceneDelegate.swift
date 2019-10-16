68e2d166a89f7bd7676fca6da4f4cdf60a6ab0ae
Heiner Gerdes <heiner.gerdes@gmx.de>
2019-10-16 12:56:01 +0200

    Version 1.0 Build 4 - Changed force cast CoreData context

--- Dynamic Predicate CoreData/SceneDelegate.swift
+++ Dynamic Predicate CoreData/SceneDelegate.swift
@@ -20,7 +20,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
         // Get the managed object context from the shared persistent container.
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        guard let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext else {
+            fatalError("Unable to read managed object context.")
+        }
 
         // Create the SwiftUI view and set the context as the value for the managedObjectContext environment keyPath.
         // Add `@Environment(\.managedObjectContext)` in the views that will need the context.
